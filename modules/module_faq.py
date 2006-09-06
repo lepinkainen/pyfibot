4828a2e6e58e805a3a3feda6a248934fae4ac763
riku.lindblad <riku.lindblad@dda364a1-ef19-0410-af65-756c83048fb2>
2006-09-06 20:54:26 +0000

    More efficient matching
    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@15 dda364a1-ef19-0410-af65-756c83048fb2

--- modules/module_faq.py
+++ modules/module_faq.py
@@ -1,10 +1,15 @@
 import os
 import os.path
 import sys
+import re
+
+# this should be faster than string.startswith
+faqregex = re.compile(r'^\?\?') # matches strings starting with ??
+faqignore = re.compile(r'(^[^\.])') # everything starting with a dot
 
 def handle_privmsg(bot, user, channel, args):
     """Usage: ?? <searchterm>"""
-    if args.startswith("??"):
+    if faqregex.match(args):
         faqdir = os.path.join(sys.path[0], "faq", channel.replace("#", ""))
         if not os.path.exists(faqdir):
             bot.say(channel, "No FAQ for this channel")
@@ -14,13 +19,15 @@ def handle_privmsg(bot, user, channel, args):
         args = args[2:].strip().replace(" ", "_")
 
 	faqs = os.listdir(faqdir)
+        faqs = filter(faqignore.match, faqs)
         if args in faqs:
             f = file(os.path.join(faqdir, args))
             value = f.read()
             f.close()
+
+            # special case
             if channel == "#wow.crew":
-                value = "!msg #wow "+value
-            bot.say(channel, value)
+                bot.say("#wow", value)
 	else:
 	    if len(faqs) > 0:
                 bot.say(channel, "FAQs for this channel: " + ", ".join(faqs))
