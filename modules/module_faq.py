8139bd52048af5761977ab06d74ee2414bdde020
riku.lindblad <riku.lindblad@dda364a1-ef19-0410-af65-756c83048fb2>
2006-09-28 19:30:37 +0000

    string.startswith is more efficient than regex matching
    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@21 dda364a1-ef19-0410-af65-756c83048fb2

--- modules/module_faq.py
+++ modules/module_faq.py
@@ -4,18 +4,21 @@ import sys
 import re
 
 # this should be faster than string.startswith
-faqregex = re.compile(r'^\?\?') # matches strings starting with ??
+#faqregex = re.compile(r'^\?\?') # matches strings starting with ??
 faqignore = re.compile(r'(^[^\.])') # everything starting with a dot
 
 def handle_privmsg(bot, user, channel, args):
     """Usage: ?? <searchterm>"""
-    if faqregex.match(args):
+
+    
+    if args.startswith("??"):
+        bot.log("Command faq called by %s" % user)
         faqdir = os.path.join(sys.path[0], "faq", channel.replace("#", ""))
         if not os.path.exists(faqdir):
             bot.say(channel, "No FAQ for this channel")
             return
 
-	# strip spaces from start & end, replace the rest with underscores
+	# skip the ?? -triggger, strip spaces from start & end, replace the rest with underscores
         args = args[2:].strip().replace(" ", "_")
 
 	faqs = os.listdir(faqdir)
@@ -25,9 +28,8 @@ def handle_privmsg(bot, user, channel, args):
             value = f.read()
             f.close()
 
-            # special case
-            if channel == "#wow.crew":
-                bot.say("#wow", value)
+            bot.say(channel, value)
+            
 	else:
 	    if len(faqs) > 0:
                 bot.say(channel, "FAQs for this channel: " + ", ".join(faqs))
