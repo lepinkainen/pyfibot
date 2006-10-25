e73a596be6b9ac00ada84172396833b553752476
riku.lindblad <riku.lindblad@dda364a1-ef19-0410-af65-756c83048fb2>
2006-10-25 23:36:14 +0000

    The serie fetching logic is a separate program now
    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@25 dda364a1-ef19-0410-af65-756c83048fb2

--- modules/module_sqliteseries.py
+++ modules/module_sqliteseries.py
@@ -1,47 +1,11 @@
 import time
 import datetime
-import urllib
-import urlparse
-import re
 import sqlite
 
 # create table series (id INTEGER PRIMARY KEY, uid TEXT UNIQUE, serie TEXT, season INTEGER, episode INTEGER, title TEXT, airdate DATE);
 # insert into series values(null, 'Alias', 1, 1, 'Pilot', date('2006-01-01'));
 # select * from series where airdate = date('2006-01-02', '-1 day');
 # select * from series where airdate = date('now', '-1 day');
-
-CREATETABLE = "CREATE TABLE series (id INTEGER PRIMARY KEY, uid TEXT UNIQUE ON CONFLICT REPLACE, serie TEXT, season INTEGER, episode INTEGER, title TEXT, airdate DATE);"
-
-def command_sqlcache(bot, user, channel, args):
-    """Refresh the whole database"""
-    if not isAdmin(user): return
-
-    for seriename in precache:
-        url = _search_serie(seriename)
-        if url != None:
-            _cache_serie(url)
-    bot.say(channel, "Caching done")
-
-def NOTinit():
-    con = sqlite.connect("/home/shrike/pyfibot/modules/series.db");
-    cur = con.cursor()
-    cur.execute(CREATETABLE)
-    cur.close()
-    con.commit()
-    con.close()
-
-def command_epdel(bot, user, channel, args):
-    """Delete all data about a serie"""
-    if not isAdmin(user): return
-
-    con = sqlite.connect("/home/shrike/pyfibot/modules/series.db");
-    cur = con.cursor()
-    cur.execute("DELETE FROM series WHERE serie='%s';" % args)
-    cur.close()
-    con.commit()
-    con.close()
-
-    bot.say(channel, "Done");
 
 def command_epinfo(bot, user, channel, args):
     """List series in the database"""
@@ -91,20 +55,7 @@ def command_ep(bot, user, channel, args):
         cur.execute("SELECT * FROM series WHERE serie LIKE %s AND airdate >= date('now', 'localtime') LIMIT 1", ("%"+args+"%",))
         # nothing found, get more data from the web
         if cur.rowcount == 0:
-            url = _search_serie(args)
-            # found, cache more episodes and try searching again
-            if url != None:
-                _cache_serie(url)
-                cur.execute("SELECT * FROM series WHERE serie LIKE %s AND airdate >= date('now', 'localtime') LIMIT 1", ("%"+args+"%",))
-                # still nothing found, give up - no new episodes
-                if cur.rowcount == 0:
-                    # get the latest actual episode
-                    #cur.execute("SELECT * FROM series WHERE serie LIKE %s ORDER BY airdate DESC LIMIT 1;", ("%"+args+"%",))                    
-                    bot.say(channel, "No future episodes of '%s' found" % args)
-                    return
-            # nothing found
-            else:
-                bot.say(channel, "Serie '%s' not found" % args)
+            bot.say(channel, "Serie '%s' not found" % args) # TODO: add to 'wishlist' file or something?
 
     episodes = []
     # go through the results
@@ -134,102 +85,3 @@ def command_ep(bot, user, channel, args):
 
     cur.close()
     con.close()
-
-def _cache_serie(url):
-    """Parse the seriedata from the internets into sqlite"""
-    
-    serie, epdata, updatedate = _get_seriedata(url)
-    con = sqlite.connect("/home/shrike/pyfibot/modules/series.db");
-    
-    for episode in epdata:
-        episode['epname'] = episode['epname'].replace("'", "pier")
-        cur = con.cursor()
-        uid = "%s%d%d" % (serie, episode['season'], episode['episode'])
-        cur.execute("INSERT INTO series (uid, serie, season, episode, title, airdate) VALUES (%s, %s, %d, %d, %s, date(%s));", (uid, serie, episode['season'], episode['episode'], episode['epname'], episode['airdate2'].strftime("%Y-%m-%d")))
-        cur.close()
-
-    # Commit & close
-    con.commit()
-    con.close()
-
-def _search_serie(searchterms):
-    """Search for a serie and return its episode list page URL"""
-
-    if not searchterms: return
-
-    # google power!
-    # Try to find the exact page by adding +"(a Titles and Air Dates Guide)" to the search
-    #url = "http://www.google.com/search?hl=en&q=site:epguides.com+%s+%%2B%%22%%28a+Titles+and+Air+Dates+Guide%%29%%22"
-    url = "http://www.google.com/search?hl=en&q=site:epguides.com%%20%s"
-    search = "%s %s"
-    search = urllib.quote(search % (searchterms, '"(a Titles and Air Dates Guide)"'))
-    bs = getUrl(url % search).getBS()
-
-    if not bs: return
-
-    results = []
-    
-    # tidy up the search results
-    for url in bs.fetch("a", {"href":re.compile("http://epguides.com/")}):
-        url = url['href']
-        
-        # only add serie summary pages (don't end with .html)
-        if url.endswith("/"):
-            results.append(url)
-
-    if not results: return None
-
-    #print "RESULTS: ", results
-    # The first result is the correct one
-    return results[0]
-
-def _get_seriedata(url):
-    """Get serie name and all episodes from the given url
-
-    @return serie name, episode data, last update day"""
-    
-    epRe=re.compile("(?P<no>[\d]+)\.[\s]+(?P<season>[\d]+)-[ ]?(?P<episode>[\d]+)[\s]+(?P<prodno>[\w-]+)?[\s]+(?P<date>[\d]{1,2} [\w]{3} [\d]{2})[\s]+<[^>]+>(?P<name>.*)</a>")
-    
-    filedata = getUrl(url).getContent()
-
-    # tidy up the crappy html a bit
-    r=[];
-    for x in [x.split('\n') for x in filedata.split('\r')]:
-        if x:
-            r += x
-
-    filedata = r
-    r = []
-    for line in filedata:
-        if line:
-            r.append(line.strip())
-
-    filedata = r
-
-    epdata = []
-
-    seriename = getUrl(url).getBS().first("h1").renderContents()
-    seriename = re.sub("<.*?>", "", seriename)
-
-    for line in filedata:
-        ###
-        # find episodes
-        ###
-        m = epRe.match(line)
-        if m:
-            
-            # convert datestring to gmtime format
-            t = time.strptime(m.group('date'), '%d %b %y')
-            
-            # put episode data into a nifty dict
-            data = {'epno'    : m.group('no'),
-                    'season'  : int(m.group('season')),
-                    'episode' : int(m.group('episode')),
-                    'prodno'  : m.group('prodno'),
-                    'airdate' : m.group('date'),
-                    'airdate2': datetime.date(t[0], t[1], t[2]),
-                    'epname'  : m.group('name')}
-        
-            epdata.append(data)
-
-    return seriename, epdata, datetime.date.today()
