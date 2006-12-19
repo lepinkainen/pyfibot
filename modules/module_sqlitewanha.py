cc5c0b6bb68237b6cf85b793e3b0a1b24b0881f6
riku.lindblad <riku.lindblad@dda364a1-ef19-0410-af65-756c83048fb2>
2006-12-19 11:07:12 +0000

    Finalized the module, should work now
    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@32 dda364a1-ef19-0410-af65-756c83048fb2

--- modules/module_sqlitewanha.py
+++ modules/module_sqlitewanha.py
@@ -1,23 +1,39 @@
 import sqlite
 import time
+import datetime
 
 # create table urls (id TEXT UNIQUE, nick TEXT, url TEXT, channel TEXT, time int);
 
 def handle_url(bot, user, channel, url):
-    print "SQLITEWANHA", user, channel, url
-
     urlid = "%s|%s" % (channel, url)
     
-
     con = sqlite.connect("/home/shrike/pyfibot/modules/urls.db")
     cur = con.cursor()
     cur.execute("SELECT * FROM urls WHERE id=%s", (urlid,))
     if cur.rowcount:
-        id, nick, url, channel, timestamp = cur.fetchone()
-        print "OLD URL", nick, timestamp
+        id, userhost, url, channel, timestamp = cur.fetchone()
+
+        pastetime = datetime.datetime.fromtimestamp(timestamp)
+        now = datetime.datetime.now()
+        age = now - pastetime
+
+        agestr = ""
+        
+        if age.days > 0:
+            agestr += "%d days " % age.days
+
+        secs = age.seconds
+        hours,minutes,seconds = secs//3600,secs//60%60,secs%60
+        
+        if hours > 0: agestr += "%d h " % hours
+        if minutes > 0: agestr += "%d m " % minutes
+        if seconds > 0: agestr += "%d s" % seconds
+
+        # don't alert for the same person
+        if getNick(user) != getNick(userhost):
+            bot.say(channel, "%s: wanha. (by %s %s ago)" % ( getNick(user), getNick(userhost), agestr))
     else:
-        print "NEW URL:", url
-        cur.execute("INSERT INTO urls VALUES(%s, %s, %s, %s, %d)", (urlid, getNick(user), url, channel, int(time.time())));
+        cur.execute("INSERT INTO urls VALUES(%s, %s, %s, %s, %d)", (urlid, user, url, channel, int(time.time())));
         
     con.commit()
     cur.close()
