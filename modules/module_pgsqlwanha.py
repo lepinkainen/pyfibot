1eec7ff52435cf8b728127915ff32f83df61e5e4
riku.lindblad <riku.lindblad@dda364a1-ef19-0410-af65-756c83048fb2>
2007-05-08 22:03:05 +0000

    PostgreSQL version of the wanha-module
    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@52 dda364a1-ef19-0410-af65-756c83048fb2

---
+++ modules/module_pgsqlwanha.py
@@ -0,0 +1,60 @@
+import sqlite
+import time
+import datetime
+from pyPgSQL import PgSQL
+
+
+# -- For postgresql
+#
+# CREATE TABLE pyfibot.urls
+# (
+#       url text NOT NULL,
+#         channel character varying(255) NOT NULL,
+#         userid character varying(255) NOT NULL,
+#         "timestamp" timestamp without time zone NOT NULL,
+#         id serial NOT NULL,
+#         CONSTRAINT id PRIMARY KEY (id)
+#       )
+# WITHOUT OIDS;
+# ALTER TABLE pyfibot.urls OWNER TO <user>;
+
+
+def handle_url(bot, user, channel, url):
+    cx = PgSQL.connect(database="shrike", host="localhost", user="shrike", password="Riku1999")
+    cur = cx.cursor()
+    # find the oldest instance of given url on this channel
+    cur.execute("SELECT * FROM pyfibot.urls WHERE channel=%s AND url=%s ORDER BY timestamp;", (channel, url))
+
+    res = cur.fetchone()
+    print res
+    if res:
+        url, channel, userhost, timestamp, urlid = res;
+
+        pastetime = datetime.datetime.fromtimestamp(res[3].ticks())
+        now = datetime.datetime.now()
+        age = now - pastetime
+
+        agestr = ""
+
+        if age.days > 0:
+            agestr += "%d days " % age.days
+
+        secs = age.seconds
+        hours,minutes,seconds = secs//3600,secs//60%60,secs%60
+
+        if hours > 0: agestr += "%d h " % hours
+        if minutes > 0: agestr += "%d m " % minutes
+        if seconds > 0: agestr += "%d s" % seconds
+
+        # don't alert for the same person
+        if getNick(user) != getNick(userhost):
+            if channel != "#wow":
+                bot.say(channel, "%s: wanha. (by %s %s ago) (PGSQL)" % ( getNick(user), getNick(userhost), agestr))
+
+    cur = cx.cursor()
+    # always store URLs, this structure can handle it, sqlite can't
+    cur.execute("INSERT INTO pyfibot.urls (userid, channel, url, timestamp) VALUES(%s, %s, %s, NOW());", (user, channel, url));
+        
+    cx.commit()
+    cur.close()
+    cx.close()
