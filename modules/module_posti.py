1709bfb0e7dc4c137d3b9f5c5c5d405abb7fa190
riku.lindblad <riku.lindblad@dda364a1-ef19-0410-af65-756c83048fb2>
2008-06-04 09:54:31 +0000

    Output UTF-8
    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@82 dda364a1-ef19-0410-af65-756c83048fb2

--- modules/module_posti.py
+++ modules/module_posti.py
@@ -5,9 +5,11 @@ import time
 baseurl = "http://www.verkkoposti.com/e3/TrackinternetServlet?lang=fi&LOTUS_hae=Hae&LOTUS_side=1&LOTUS_trackId=%s&LOTUS_hae=Hae"
 
 def command_posti(bot, user, channel, args):
-    """Parse a webhcat hex ip to a domain"""
+    """Get the latest status for a package"""
     result = getstatus(args, count=1)
-    bot.say(channel, result)
+    
+    for line in result:
+        bot.say(channel, line.encode("UTF-8"))
 
 def getstatus(code, count=None):
     url = baseurl % code
@@ -26,7 +28,6 @@ def getstatus(code, count=None):
         date = time.strptime(date, "%d.%m.%Y, klo %H:%M&nbsp;")
         location = location[6:].strip()
 
-        
         res.append("%s - %s - %s" % (time.strftime("%Y-%m-%d %H:%M:%S", date), statustext, location))
 
     if count:
