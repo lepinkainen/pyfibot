7d1e5d86a161c558520fea0f7d2b6bbf712d86d0
riku.lindblad <riku.lindblad@dda364a1-ef19-0410-af65-756c83048fb2>
2008-06-04 09:39:14 +0000

    Added module for package tracking
    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@81 dda364a1-ef19-0410-af65-756c83048fb2

---
+++ modules/module_posti.py
@@ -0,0 +1,35 @@
+import urllib2
+from BeautifulSoup import BeautifulSoup
+import time
+
+baseurl = "http://www.verkkoposti.com/e3/TrackinternetServlet?lang=fi&LOTUS_hae=Hae&LOTUS_side=1&LOTUS_trackId=%s&LOTUS_hae=Hae"
+
+def command_posti(bot, user, channel, args):
+    """Parse a webhcat hex ip to a domain"""
+    result = getstatus(args, count=1)
+    bot.say(channel, result)
+
+def getstatus(code, count=None):
+    url = baseurl % code
+    f = urllib2.urlopen(url)
+    d = f.read()
+    f.close()
+
+    bs = BeautifulSoup(d)
+
+    res = []
+
+    statuslist = bs.find("div", {'class':"result_up"}).find("table", {'width': '500'}).findAll("p", {'class': 'resulttext'})
+    for status in statuslist:
+        date, statustext, location = status.contents
+        statustext = statustext.string
+        date = time.strptime(date, "%d.%m.%Y, klo %H:%M&nbsp;")
+        location = location[6:].strip()
+
+        
+        res.append("%s - %s - %s" % (time.strftime("%Y-%m-%d %H:%M:%S", date), statustext, location))
+
+    if count:
+        return res[:count]
+    else:
+        return res
