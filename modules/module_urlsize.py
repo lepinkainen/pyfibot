7b382fbe3ded1da9b9b7ceeea15886824cb29033
riku.lindblad <riku.lindblad@dda364a1-ef19-0410-af65-756c83048fb2>
2008-11-27 12:00:42 +0000

    Now displays content-typ
    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@92 dda364a1-ef19-0410-af65-756c83048fb2

--- modules/module_urlsize.py
+++ modules/module_urlsize.py
@@ -6,10 +6,10 @@ def handle_url(bot, user, channel, url, msg):
 
     # inform about large files (over 5MB)
     size = getUrl(url).getSize()
-
+    contentType = getUrl(url).getHeaders()['content-type']
     if not size: return
     
     size = size / 1024
     
     if size > 5:
-        bot.say(channel, "File size: %s MB" % size)
+        bot.say(channel, "File size: %s - MB Content-Type: %s" % (size, contentType))
