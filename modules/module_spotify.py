a2fd2436cb1c0285dfdd18fad43e505d7c246535
riku.lindblad <riku.lindblad@dda364a1-ef19-0410-af65-756c83048fb2>
2009-08-16 21:31:41 +0000

    Handle spotify: -type urls Cleanup
    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@144 dda364a1-ef19-0410-af65-756c83048fb2

--- modules/module_spotify.py
+++ modules/module_spotify.py
@@ -1,15 +1,7 @@
-
 import re
 import urllib
 
-def handle_url(bot, user, channel, url, msg):
-    """Handle IMDB urls"""
-    
-    m = re.match("(http:\/\/open.spotify.com\/|spotify:)(album|artist|track)([:\/])([a-zA-Z0-9]+)\/?", url)
-    if not m: return
-
-    dataurl = "http://spotify.url.fi/%s/%s?txt" % (m.group(2), m.group(4))
-
+def do_spotify(bot, user, channel, dataurl):    
     f = urllib.urlopen(dataurl)
     songinfo = f.read()
     f.close()
@@ -17,3 +9,13 @@ def handle_url(bot, user, channel, url, msg):
     artist, album, song = songinfo.split("/", 2)
 
     bot.say(channel, "[Spotify] %s - %s (%s)" % (artist.strip(), song.strip(), album.strip()))
+
+def handle_privmsg(bot, user, reply, msg):
+    """Grab Spotify URLs from the messages and handle them"""
+
+    m = re.match("(http:\/\/open.spotify.com\/|spotify:)(album|artist|track)([:\/])([a-zA-Z0-9]+)\/?", msg)
+    if not m: return
+
+    dataurl = "http://spotify.url.fi/%s/%s?txt" % (m.group(2), m.group(4))
+
+    do_spotify(bot, user, reply, dataurl)
