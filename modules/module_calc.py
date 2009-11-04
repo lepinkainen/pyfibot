ca1c6a8c7fbcdf53b269fccd82e935c198ea41ac
riku.lindblad <riku.lindblad@dda364a1-ef19-0410-af65-756c83048fb2>
2009-11-04 13:34:37 +0000

    Use Google as a calculator
    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@157 dda364a1-ef19-0410-af65-756c83048fb2

---
+++ modules/module_calc.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import re
+from operator import pow, add, sub, mul, div, mod
+
+import urllib2
+
+def doTheMath(matchobj):
+    e = matchobj.groups()
+    res = {"^": pow, "+": add, "-": sub, "*": mul, "/": div, "%": mod}[e[1]](float(e[0]), float(e[2]))
+    return str(res)
+
+def calc(str, match=True):
+    if match: str = str.group(0)[1:-1]
+
+    while True:
+        newstr = re.sub("[(][-+*^%/\d.]*[)]", calc, str)
+        if (newstr == str): break
+        else: str = newstr
+
+    for op in ["[\^]", "[*/%]", "[-+]"]:
+        while True:
+            newstr = re.sub("(\-?[\d.]+)(%s)(\-?[\d.]+)" % op, doTheMath, str)
+            if (newstr == str): break
+            else: str = newstr
+
+    return str
+
+def calc_google(args):
+    google_url = "http://www.google.com/search?hl=en&num=1&q=%s"
+    search_url = google_url % urllib2.quote(args)
+
+    request = urllib2.Request(search_url)
+    request.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.4) Gecko/20091016 Firefox/3.5.4')
+
+    opener = urllib2.build_opener()
+
+    d = opener.open(request).read()
+
+    m = re.search("\/calc_img\.gif.*?<b>(.*?)<\/b>", d)
+    if m:
+        return m.group(1)
+    else:
+        return "Invalid calculation"
+
+def command_calc(bot, user, channel, args):
+    if not args: return
+    bot.say(channel, calc_google(args))
