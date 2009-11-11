5f15bd55989754b1818d5d546902e23adc42fd70
Samuli.Lager@gmail.com <Samuli.Lager@gmail.com@dda364a1-ef19-0410-af65-756c83048fb2>
2009-11-11 17:53:02 +0000

    edited name of the module from bmj to bmi (this was typo)
    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@159 dda364a1-ef19-0410-af65-756c83048fb2

--- modules/module_bmi.py
+++ modules/module_bmi.py
@@ -1,28 +1,28 @@
-def calc_bmj(height, weight):
+def calc_bmi(height, weight):
     return  (float(weight)/height**2)*10000
 
-def print_bmj(bmj):
-    if bmj < 16.5:
+def print_bmi(bmi):
+    if bmi < 16.5:
         weight_category = "severely underweight (less than 16.5)"
-    elif bmj < 18.5:
+    elif bmi < 18.5:
         weight_category = "underweight (from 16.5 to 18.4)"
-    elif bmj < 25:
+    elif bmi < 25:
         weight_category = "normal (from 18.5 to 24.9)"
-    elif bmj < 30.1:
+    elif bmi < 30.1:
         weight_category = "overweight (from 25 to 30)"
-    elif bmj < 35:
+    elif bmi < 35:
         weight_category = "obese class I (from 30.1 to 34.9)"
-    elif bmj <= 40:
+    elif bmi <= 40:
         weight_category = "obese class II (from 35 to 40)"
     else:
         weight_category = "obese class III (over 40)"
 
-    return("your BMJ is %.2f which is %s" % (bmj, weight_category))
+    return("your bmi is %.2f which is %s" % (bmi, weight_category))
 
-def command_bmj(bot, user, channel, args):
-    """Calculates your body mass index. Usage: .bmj height(cm)/weight(kg)"""
+def command_bmi(bot, user, channel, args):
+    """Calculates your body mass index. Usage: .bmi height(cm)/weight(kg)"""
     data = args.split("/")
     if len(data) == 2:
-        bot.say(channel, print_bmj(calc_bmj(int(data[0]), int(data[1]))))
+        bot.say(channel, print_bmi(calc_bmi(int(data[0]), int(data[1]))))
     else:
         bot.say(channel, "error with arguments")
