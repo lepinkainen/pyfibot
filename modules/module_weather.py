56a6da7d1555c35ce2e54a0141ef66125b236b6b
Samuli.Lager@gmail.com <Samuli.Lager@gmail.com@dda364a1-ef19-0410-af65-756c83048fb2>
2010-02-09 11:05:12 +0000

    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@190 dda364a1-ef19-0410-af65-756c83048fb2

--- modules/module_weather.py
+++ modules/module_weather.py
@@ -31,7 +31,10 @@ def command_weather(bot, user, channel, args):
         return
 
     compass_point, kmph, mps = parse_google_wind_condition(result_dict['current_conditions']['wind_condition'])
-	compass_point = {'N': 'north', 'S': 'south', 'W': 'west', 'E': 'east', 'NW': 'northwest', 'NE': 'northeast', 'SW': 'southwest', 'SE': 'southeast'}[compass_point]
+	
+	compass_points = {'N': 'north', 'S': 'south', 'W': 'west', 'E': 'east', 'NW': 'northwest', 'NE': 'northeast', 'SW': 'southwest', 'SE': 'southeast'}
+	compass_point = compass_points[compass_point]
+
     city = result_dict['forecast_information']['city']
     condition = result_dict['current_conditions']['condition']
     temperature = int(result_dict['current_conditions']['temp_c'])
