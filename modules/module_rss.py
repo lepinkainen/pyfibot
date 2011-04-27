2acb73d4be8d13a08a9b0fd4d9d2a7a3b3bbb2a7
tominyk <tominyk@dda364a1-ef19-0410-af65-756c83048fb2>
2011-04-27 10:15:21 +0000

    Updated shorturl function to use bit.ly service. Added more output syntaxes. Fixed rotators, so those won't run if database is empty. Added new .rss set subcommand which could be used to change feed's settings
    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@283 dda364a1-ef19-0410-af65-756c83048fb2

--- modules/module_rss.py
+++ modules/module_rss.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""pyfibot BETA module_rss.py
+"""pyfibot module_rss.py
 @author Henri 'fgeek' Salo <henri@nerv.fi>, Tomi 'dmc' Nykänen
 @copyright Copyright (c) 2010 Henri Salo
 @licence BSD
@@ -21,6 +21,9 @@ try:
     import sqlite3
     from twisted.internet import reactor
     import yaml
+    import urllib2
+    import simplejson as json
+    import htmlentitydefs
     init_ok = True
 except ImportError, error:
     print 'Error starting rss module: ',error
@@ -31,13 +34,14 @@ log = logging.getLogger('rss')
 
 t = None
 t2 = None
-
 # Config format:
 # database: rss.db
 # delays:
 #   rss_sync: 300 #How often we synchronize rss-feeds (in seconds)
 #   output: 7 #How often to output new elements to channels
-# output_syntax: 0 #0 == feed_title: title - url, 1 == feed_title: title - shorturl, 2 == feed_title: title (id)
+# output_syntax: 0 #0 == feed_title: title - url, 1 == feed_title: title - shorturl, 2 == feed_title: title (id), 3 == feed_title: title, 4 == title
+# bitly_login: #Needed if using shorturl format
+# bitly_api_key: #Needed if using shorturl format
 
 # With output_syntax #2 you could get url via .url <id>
 
@@ -46,12 +50,9 @@ def event_signedon(bot):
     if not init_ok: 
         log.error("Config not ok, not starting rotators")
         return False
-
-    global delay
-    global output_dealy
-
-    rotator_indexfeeds(bot, rssconfig["delays"]["rss_sync"])
-    rotator_output(bot, rssconfig["delays"]["output"])
+    if (empty_database > 0):
+        rotator_indexfeeds(bot, rssconfig["delays"]["rss_sync"])
+        rotator_output(bot, rssconfig["delays"]["output"])
 
 def init(botconfig):
     """Creates database if it doesn't exist"""
@@ -66,16 +67,19 @@ def init(botconfig):
 
     db_conn = sqlite3.connect(rssconfig["database"])
     d = db_conn.cursor()
-    d.execute("CREATE TABLE IF NOT EXISTS feeds (id INTEGER PRIMARY KEY,feed_url TEXT,channel TEXT,feed_title TEXT);")
+    d.execute("CREATE TABLE IF NOT EXISTS feeds (id INTEGER PRIMARY KEY,feed_url TEXT,channel TEXT,feed_title TEXT, output_syntax_id INTEGER);")
     d.execute("CREATE TABLE IF NOT EXISTS titles_with_urls (id INTEGER PRIMARY KEY,feed_url TEXT,title TEXT,url TEXT,channel TEXT,printed INTEGER,hash TEXT UNIQUE);")
     db_conn.commit()
+    #Check if database is empty
+    global empty_database
+    empty_database = d.execute("SELECT COUNT(*) FROM feeds").fetchone()[0]
     d.close()
-    
-def rss_addfeed(bot, user, channel, args):
+   
+def rss_addfeed(bot, user, channel, feed_url, output_syntax):
     """Adds RSS-feed to sqlite-database"""
-    url = args
+    global empty_database
     try:
-        feed_data = feedparser.parse(url)
+        feed_data = feedparser.parse(feed_url)
         feed_title = feed_data['feed']['title']
     except KeyError, e:
         return bot.say(channel, "Nothing inserted to database. Probably you mistyped URL?")
@@ -95,17 +99,21 @@ def rss_addfeed(bot, user, channel, args):
     except Exception, e:
         bot.say(channel, "Error: %s" % e)
         
-    d.execute("SELECT * FROM feeds WHERE feed_url = ? AND channel = ?", (url, channel, ))
+    d.execute("SELECT * FROM feeds WHERE feed_url = ? AND channel = ?", (feed_url, channel, ))
     already_on_db = d.fetchone()
     if already_on_db is None:
-            data = [None, url, channel, feed_title]
-            d.execute("INSERT INTO feeds VALUES (?, ?, ?, ?)", data)
+            data = [None, feed_url, channel, feed_title, output_syntax]
+            d.execute("INSERT INTO feeds VALUES (?, ?, ?, ?, ?)", data)
             db_conn.commit()
             d.close()
-            return bot.say(channel, "Url \"%s\" inserted to database." % url)
+            if (empty_database == 0):
+                rotator_indexfeeds(bot, rssconfig["delays"]["rss_sync"])
+                rotator_output(bot, rssconfig["delays"]["output"])
+                empty_database = 1
+            return bot.say(channel, "Url \"%s\" inserted to database." % feed_url)
     else:
         id = already_on_db[0] 
-        return bot.say(channel, "Url \"%s\" is already on database with ID: %i" % (url, id))
+        return bot.say(channel, "Url \"%s\" is already on database with ID: %i" % (feed_url, id))
 
 def rss_delfeed(bot, user, channel, args):
     """Deletes RSS-feed from sqlite-database by given ID"""
@@ -143,6 +151,24 @@ def list_feeds(channel):
     d.close()
     return feeds
 
+def rss_modify_feed_setting(bot, feed_ident, channel, target, tvalue):
+    """Modifies feed's settings"""
+    db_conn = sqlite3.connect(rssconfig["database"])
+    d= db_conn.cursor()
+    d.execute("SELECT id, feed_url FROM feeds WHERE id = ? OR feed_url = ?", (feed_ident, feed_ident,))
+    row = d.fetchone()
+    feed_url = row[1].encode("UTF-8")
+    if (target == "title"):
+        if (db_conn.execute("UPDATE feeds SET feed_title = ? WHERE id = ? OR feed_url = ? AND channel = ?", (tvalue, feed_ident, feed_ident, channel)).rowcount == 1):
+            db_conn.commit()
+            bot.say(channel, "Feed's (%s) title modified to \"%s\"" % (feed_url, tvalue.encode("UTF-8")))
+    elif (target == "syntax"):
+        if (db_conn.execute("UPDATE feeds SET output_syntax_id = ? WHERE id = ? OR feed_url = ? AND channel = ?", (tvalue, feed_ident, feed_ident, channel)).rowcount == 1):
+            db_conn.commit()
+            bot.say(channel, "Feed's (%s) output syntax modified to \"%s\"" % (feed_url, tvalue.encode("UTF-8"))) 
+    else: bot.say(channel, "Invalid syntax! Usage: Add feed: .rss add <feed_url>, Delete feed: .rss del <feed_url/feed_id>, List feeds: .rss list, Change feed settings: .rss set <feed_id/feed_url> title/syntax <value>")
+    d.close()
+    
 def rss_listfeeds(bot, user, channel, args):
     """Lists all RSS-feeds added to channel"""
     feeds = list_feeds(channel)
@@ -150,27 +176,61 @@ def rss_listfeeds(bot, user, channel, args):
         bot.say(channel, "%s: %s" % (feed[0], feed[1]))
 
 def command_rss(bot, user, channel, args):
-    """Usage: Add feed: .rss add <feed_url>, Delete feed: .rss del <feed_url/feed_id>, List feeds: .rss list"""
+    """Usage: Add feed: .rss add <feed_url>, Delete feed: .rss del <feed_url/feed_id>, List feeds: .rss list, Change feed settings: .rss set <feed_id/feed_url> title/syntax <value>"""
     try:
         args = args.split()
         subcommand = args[0]
         if (subcommand != "list"): feed_ident = args[1]
         if (isAdmin(user)):
             if (subcommand == "add"):
-                rss_addfeed(bot, user, channel, feed_ident)
+                if (len(args) > 2): output_syntax = args[2]
+                else: output_syntax = rssconfig["output_syntax"]
+                rss_addfeed(bot, user, channel, feed_ident, output_syntax)
             elif (subcommand == "del"):
                 rss_delfeed(bot, user, channel, feed_ident)
             elif (subcommand == "list"):
                 rss_listfeeds(bot, user, channel, None)
+            elif (subcommand == "set"):
+                target = args[2]
+                tvalue = args[3].decode("UTF-8")
+                if (target == "title"): rss_modify_feed_setting(bot, feed_ident, channel, "title", tvalue)
+                elif (target == "syntax"): rss_modify_feed_setting(bot, feed_ident, channel, "syntax", tvalue)
             else:
-                bot.say(channel, "Invalid syntax! Usage: Add feed: .rss add <feed_url>, Delete feed: .rss del <feed_url/feed_id>, List feeds: .rss list")
+                bot.say(channel, "Invalid syntax! Usage: Add feed: .rss add <feed_url>, Delete feed: .rss del <feed_url/feed_id>, List feeds: .rss list, Change feed settings: .rss set <feed_id/feed_url> title/syntax <value>")
     except IndexError:
-        bot.say(channel, "Invalid syntax! Usage: Add feed: .rss add <feed_url>, Delete feed: .rss del <feed_url/feed_id>, List feeds: .rss list") 
+        bot.say(channel, "Invalid syntax! Usage: Add feed: .rss add <feed_url>, Delete feed: .rss del <feed_url/feed_id>, List feeds: .rss list, Change feed settings: .rss set <feed_id/feed_url> title/syntax <value>") 
 
 def shorturl(url):
-    params = urllib.urlencode({'create': url})
-    shorturl = urllib.urlopen("http://href.fi/api.php?%s" % params)
-    return shorturl.read()
+    try:
+        req = urllib2.Request("http://api.bit.ly/v3/shorten?%s" % urllib.urlencode({'longUrl': url, 'login': rssconfig["bitly_login"], 'apiKey': rssconfig["bitly_api_key"], 'format': 'json'}))
+        results = json.loads(urllib2.urlopen(req).read())
+        #log.debug("Shorturl: %s" % results['id'].encode("UTF-8"))
+        if (results['status_code'] == 200):
+            return results['data']['url'].encode("UTF-8")
+        raise Exception("Error in function shorturl: %s" % results['status_txt'])
+    except HTTPError, e:
+        log.debug('Error in function shorturl (url => %s): %s' % (url,e.read()))
+
+def unescape(text): # Unescape ugly wtf-8-hex-escaped chars
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text # leave as is
+    return re.sub("&#?\w+;", fixup, text)
 
 def remove_html_tags(data):
     p = re.compile(r'<.*?>')
@@ -201,20 +261,15 @@ def sqlite_add_item(bot, feed_url, title, url, channel, cleanup):
 def indexfeeds(bot):
     """Updates all RSS-feeds found on database and outputs new elements"""
     try:
-	cleanup = 0
+        cleanup = 0
         log.debug("indexfeeds thread started")
         db_conn = sqlite3.connect(rssconfig["database"])
         d = db_conn.cursor()
 	
         # If table is removed, then create a new one
         titles_table_exists = d.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='titles_with_urls'").fetchone()
-	if (titles_table_exists[0] == 0):
+    	if (titles_table_exists[0] == 0):
             init(None)
-            #cleanup = 1
-	# If titles table is empty, insert new feed elements as "printed", so bot won't flood on startup
-	#titles_count = d.execute("SELECT count(*) FROM titles_with_urls").fetchone()
-        #if (titles_count[0] == 0):
-        #    cleanup = 1
 
         # Let's count all rows to index
         rowcount = 0
@@ -251,7 +306,7 @@ def indexfeeds(bot):
         log.debug('Error in indexfeeds: %s', e)
 
 def command_url(bot, user, channel, args):
-    """Prints feed elements url by given id"""
+    """Prints feed's element url by given id"""
     id = args
     try:
         db_conn = sqlite3.connect(rssconfig["database"])
@@ -280,22 +335,25 @@ def output(bot):
         if (row != None):
             id = row[0]
             feed_url = row[1]
+            feed_output_syntax = d.execute("SELECT output_syntax_id FROM feeds WHERE feed_url = ?", (feed_url,)).fetchone()[0]
             title = row[2]
             url = row[3]
             channel = row[4]
-            title = title.encode("UTF-8")
+            title = unicode(unescape(title)).encode("UTF-8")
             channel = channel.encode("UTF-8")
             url = url.encode("UTF-8")
             feed_title = d.execute("SELECT feed_title from feeds where feed_url = ?", (feed_url,)).fetchone()[0].encode('UTF-8')
-
-            if (rssconfig["output_syntax"] == 0): 
-                bot.say(channel, "%s: %s - %s" % (feed_title, title, url))
-            elif (rssconfig["output_syntax"] == 1): 
-                bot.say(channel, "%s: %s - %s" % (feed_title, title, shorturl(url)))
-            elif (rssconfig["output_syntax"] == 2): 
+            if (feed_output_syntax == 0): 
+                bot.say(channel, "%s: %s – %s" % (feed_title, title, url))
+            elif (feed_output_syntax == 1): 
+                bot.say(channel, "%s: %s – %s" % (feed_title, title, shorturl(url)))
+            elif (feed_output_syntax == 2): 
                 bot.say(channel, "%s: %s (%i)" % (feed_title, title, id))
+            elif (feed_output_syntax == 3): 
+                bot.say(channel, "%s: %s" % (feed_title, title))
+            elif (feed_output_syntax == 4): 
+                bot.say(channel, "%s" % title)
             data = [url, channel]
-
             d.execute("UPDATE titles_with_urls SET printed=1 WHERE URL=? and channel=?", data)
             db_conn.commit()
             log.debug("output thread terminated cleanly")
@@ -309,7 +367,6 @@ def output(bot):
 def rotator_indexfeeds(bot, delay):
     """Timer for methods/functions"""
     try:
-        print "indexfeeds run"
         global t, t2
         if (type(t2).__name__ == 'NoneType'):
             t = Thread(target=indexfeeds, args=(bot,))
@@ -319,7 +376,8 @@ def rotator_indexfeeds(bot, delay):
             t = Thread(target=indexfeeds, args=(bot,))
             t.daemon = True
             t.start()
-        reactor.callLater(delay, rotator_indexfeeds, bot, delay)
+        if (empty_database > 0):
+            reactor.callLater(delay, rotator_indexfeeds, bot, delay)
     except Exception, e:
         print e
 
@@ -332,6 +390,7 @@ def rotator_output(bot, delay):
             t2.daemon = True
             t2.start()
             t2.join()
-        reactor.callLater(delay, rotator_output, bot, delay)
+        if (empty_database > 0):
+            reactor.callLater(delay, rotator_output, bot, delay)
     except Exception, e:
         print e
