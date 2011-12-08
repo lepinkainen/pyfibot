cfe7a819e0c234da6b092ad1e34dbd49f1610fc1
riku.lindblad <riku.lindblad@dda364a1-ef19-0410-af65-756c83048fb2>
2011-12-08 11:55:08 +0000

    Add xxx tld
    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@316 dda364a1-ef19-0410-af65-756c83048fb2

--- util/pyfiurl.py
+++ util/pyfiurl.py
@@ -41,7 +41,7 @@ _countrycodes=['ac', 'ad', 'ae', 'af', 'ag', 'ai', 'al', 'am', 'an', 'ao',
                'uy', 'uz', 'va', 'vc', 've', 'vg', 'vi', 'vn', 'vu', 'wf',
                'ws', 'ye', 'yt', 'yu', 'za', 'zm', 'zw', 'aero', 'biz', 'com',
                'coop', 'edu', 'gov', 'info', 'int', 'mil', 'museum', 'name',
-               'net', 'org', 'pro', 'me', 'mobi']
+               'net', 'org', 'pro', 'me', 'mobi', 'xxx']
 
 _schemes=['http://', 'https://', 'ftp://', 'svn://']
 
