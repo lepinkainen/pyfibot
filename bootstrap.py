bd31f17a3368528d369587256050aa8ffcf9f040
riku.lindblad@gmail.com <riku.lindblad@gmail.com@dda364a1-ef19-0410-af65-756c83048fb2>
2012-11-13 13:20:10 +0000

    Update bootstrap script
    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@339 dda364a1-ef19-0410-af65-756c83048fb2

--- bootstrap.py
+++ bootstrap.py
@@ -1151,6 +1151,7 @@ def after_install(options, home_dir):
     subprocess.call([join(bin_dir, 'easy_install'), 'requests'])
     subprocess.call([join(bin_dir, 'easy_install'), 'pyyaml'])
     subprocess.call([join(bin_dir, 'easy_install'), 'imdbpy'])
+    subprocess.call([join(bin_dir, 'easy_install'), 'beautifulsoup'])
     subprocess.call([join(bin_dir, 'paver'),'prepare'])
 
 ##file site.py
