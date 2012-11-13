8870597fbe31b76cb34114eb0268005e87a4052c
riku.lindblad@gmail.com <riku.lindblad@gmail.com@dda364a1-ef19-0410-af65-756c83048fb2>
2012-11-13 11:36:21 +0000

    More paver setup
    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@335 dda364a1-ef19-0410-af65-756c83048fb2

--- pavement.py
+++ pavement.py
@@ -1,13 +1,29 @@
 import os
 from paver.easy import *
+import paver.virtual
+import paver.setuputils
+from paver import svn
+from paver.setuputils import setup, find_package_data, find_packages
 
 options(
     #  bootstrap=Bunch(bootstrap_dir="bootstrap"),
       virtualenv=Bunch(
               packages_to_install=[],
+              paver_command_line='develop',
               no_site_packages=True
           )
     )
+
+install_requires = ['twisted', 'requests']
+
+setup(
+      name="PyFiBot",
+      version="0.8",
+      packages="pyfibot",
+      description="The programmers' IRC bot",
+      license="BSD",
+      install_requires=install_requires
+      )
 
 
 @task
@@ -24,6 +40,5 @@ def bootstrap(options):
 
     options.virtualenv.no_site_packages = False
     options.bootstrap.no_site_packages = False
-    options.virtualenv.paver_command_line='prepare'
+    #options.virtualenv.paver_command_line='prepare'
     call_task('paver.virtual.bootstrap')
-
