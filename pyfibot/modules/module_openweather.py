c541e9d2e5b58295dac814afc2d3c4536c06208f
kipenroskaposti@gmail.com <kipenroskaposti@gmail.com@dda364a1-ef19-0410-af65-756c83048fb2>
2012-12-11 14:49:47 +0000

    Added threshold to show measurement time, if measurement time older than threshold. Shows measurement time in UTC.
    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@366 dda364a1-ef19-0410-af65-756c83048fb2

--- pyfibot/modules/module_openweather.py
+++ pyfibot/modules/module_openweather.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import requests
 import logging
+from datetime import datetime, timedelta
 
 
 log = logging.getLogger('openweather')
@@ -9,13 +10,16 @@ url = 'http://openweathermap.org/data/2.1/find/name?q=%s'
 
 def init(bot):
     global default_location
+    global threshold
     config = bot.config.get("module_openweather", {})
     default_location = config.get("default_location", "Helsinki")
+    threshold = int(config.get("threshold", 120))  # threshold to show measuring time in minutes
     log.info('Using %s as default location' % default_location)
 
 
 def command_weather(bot, user, channel, args):
     global default_location
+    global threshold
     if args:
         location = args
     else:
@@ -30,8 +34,11 @@ def command_weather(bot, user, channel, args):
             pressure = data['main']['pressure']  # Atmospheric pressure in hPa
             wind = data['wind']['speed']  # Wind speed in mps (m/s)
             cloudiness = data['clouds']['all']  # Cloudiness in %
-
-            text = '%s: Temperature: %.1fc, Humidity: %d%%, Pressure: %d hPa, Wind: %.1f m/s, Cloudiness: %d%%' % (location, temperature, humidity, pressure, wind, cloudiness)
+            measured = datetime.utcfromtimestamp(data['dt'])
+            if datetime.utcnow() - timedelta(minutes=threshold) > measured:  # Shows measurement time if older than threshold
+                text = '%s (%s UTC): Temperature: %.1fc, Humidity: %d%%, Pressure: %d hPa, Wind: %.1f m/s, Cloudiness: %d%%' % (location, measured.strftime('%Y-%m-%d %H:%M'), temperature, humidity, pressure, wind, cloudiness)
+            else:
+                text = '%s: Temperature: %.1fc, Humidity: %d%%, Pressure: %d hPa, Wind: %.1f m/s, Cloudiness: %d%%' % (location, temperature, humidity, pressure, wind, cloudiness)
             text = text.encode('utf-8')
             return bot.say(channel, text)
     else:
