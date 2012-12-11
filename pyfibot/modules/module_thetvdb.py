93ef4b72c376416be364b2618a955308ffd35837
riku.lindblad@gmail.com <riku.lindblad@gmail.com@dda364a1-ef19-0410-af65-756c83048fb2>
2012-12-11 14:41:57 +0000

    Better error handling, handle show not found and no airdate found -cases
    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@365 dda364a1-ef19-0410-af65-756c83048fb2

--- pyfibot/modules/module_thetvdb.py
+++ pyfibot/modules/module_thetvdb.py
@@ -2,22 +2,24 @@
 
 from datetime import datetime, timedelta
 import tvdb_api
-
+import tvdb_exceptions
 
 def command_ep(bot, user, channel, args):
     t = tvdb_api.Tvdb()
     now = datetime.now()
 
-    series = t[args]
+    try:
+        series = t[args]
+    except tvdb_exceptions.tvdb_shownotfound:
+        bot.say(channel, "Series '%s' not found" % args)
+        return
+
     latest_season = series[series.keys()[-1]]
 
-    for episode_no in latest_season.keys():
-        episode = latest_season[episode_no]
+    for episode_no, episode in latest_season.items():
         firstaired = episode['firstaired']
 
         if not firstaired:
-            msg = "No new episode airdates found for %s" % series.data['seriesname']
-            bot.say(channel, msg.encode("UTF-8"))
             break
 
         airdate = datetime.strptime(firstaired, "%Y-%m-%d")
@@ -26,4 +28,8 @@ def command_ep(bot, user, channel, args):
         if td > timedelta(0, 0, 0):
             msg = "Next episode of %s '%s' airs %s (%d days)" % (series.data['seriesname'], episode['episodename'], episode['firstaired'], td.days)
             bot.say(channel, msg.encode("UTF-8"))
-            break
+            return
+
+    msg = "No new episode airdates found for %s" % series.data['seriesname']
+    bot.say(channel, msg.encode("UTF-8"))
+
