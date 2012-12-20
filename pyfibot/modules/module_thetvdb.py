ee3634fbee7e0bd311337007743b30934aca73ba
riku.lindblad@gmail.com <riku.lindblad@gmail.com@dda364a1-ef19-0410-af65-756c83048fb2>
2012-12-20 12:02:16 +0000

    Fix episode finding logic to handle specials and cases where episodes are out of order in tvdb api result
    git-svn-id: http://pyfibot.googlecode.com/svn/trunk@374 dda364a1-ef19-0410-af65-756c83048fb2

--- pyfibot/modules/module_thetvdb.py
+++ pyfibot/modules/module_thetvdb.py
@@ -14,22 +14,30 @@ def command_ep(bot, user, channel, args):
         bot.say(channel, "Series '%s' not found" % args)
         return
 
-    latest_season = series[series.keys()[-1]]
+    episodes = []
 
-    for episode_no, episode in latest_season.items():
-        firstaired = episode['firstaired']
+    # find all episodes with airdate > now
+    for season_no, season in series.items():
+        for episode_no, episode in season.items():
+            firstaired = episode['firstaired']
+            if not firstaired:
+                continue
+            airdate = datetime.strptime(firstaired, "%Y-%m-%d")
+            td = airdate - now
+            # find the next unaired episode
+            if td > timedelta(0, 0, 0):
+                episodes.append(episode)
 
-        if not firstaired:
-            break
+    # if any episodes were found, find out the one with airdate closest to now
+    if episodes:
+        # sort the list just in case
+        episodes = sorted(episodes, key=firstaired)
+        episode = episodes[0]
+        td = datetime.strptime(episode['firstaired'], "%Y-%m-%d") - now
 
-        airdate = datetime.strptime(firstaired, "%Y-%m-%d")
-        td = airdate - now
-        # find the next unaired episode
-        if td > timedelta(0, 0, 0):
-            msg = "Next episode of %s '%s' airs %s (%d days)" % (series.data['seriesname'], episode['episodename'], episode['firstaired'], td.days)
-            bot.say(channel, msg.encode("UTF-8"))
-            return
-
-    msg = "No new episode airdates found for %s" % series.data['seriesname']
-    bot.say(channel, msg.encode("UTF-8"))
+        msg = "Next episode of %s '%s' airs %s (%d days)" % (series.data['seriesname'], episode['episodename'], episode['firstaired'], td.days)
+        bot.say(channel, msg.encode("UTF-8"))
+    else:
+        msg = "No new episode airdates found for %s" % series.data['seriesname']
+        bot.say(channel, msg.encode("UTF-8"))
 
