3663a732727d2a2198b2fe1c77a3d73abb531d8e
Riku Lindblad <riku.lindblad@gmail.com>
2013-07-11 15:10:56 +0300

    Add utility to generate bearer keys for twitter

---
+++ pyfibot/util/twitter_application_auth.py
@@ -0,0 +1,22 @@
+import requests
+import base64
+import sys
+
+if len(sys.argv) < 3:
+    print "Usage: twitter_application_auth.py <consumer key> <consumer secret>"
+    sys.exit(1)
+
+consumer_key = sys.argv[1]
+consumer_secret = sys.argv[2]
+token = consumer_key+":"+consumer_secret
+encoded_token = base64.b64encode(token)
+
+payload = {'grant_type': 'client_credentials'}
+headers = {'Authorization': 'Basic '+encoded_token}
+auth_url = "https://api.twitter.com/oauth2/token"
+r = requests.post(auth_url, payload, headers=headers)
+bearer_token = r.json()['access_token']
+
+print "Bearer token:"
+print bearer_token
+
