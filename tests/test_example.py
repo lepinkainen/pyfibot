b6a1fa745ed0a5f3b2c7e272ed03a8bab0c6ec3a
Kimmo Huoman <kipenroskaposti@gmail.com>
2013-10-22 15:02:46 +0300

    empty line at end of file...

--- tests/test_example.py
+++ tests/test_example.py
@@ -16,4 +16,3 @@ def test_module_bmi():
     from pyfibot.modules import module_bmi
     bot = bot_mock.BotMock()
     eq_(('#channel', 'your bmi is 27.76 which is overweight (from 25 to 30)'), module_bmi.command_bmi(bot, None, "#channel", "175/85"))
-
