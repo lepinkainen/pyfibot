edb288676d42cc62321285595434de75a56e233f
Kimmo Huoman <kipenroskaposti@gmail.com>
2013-10-28 15:59:15 +0200

    Moved _to_unicode and _to_utf8 to PyFiBotFactory. This allows the functions to be used in modules.

--- tests/bot_mock.py
+++ tests/bot_mock.py
@@ -15,3 +15,21 @@ class BotMock(botcore.CoreCommands):
     def say(self, channel, message, length=None):
         #return("%s|%s" % (channel, message))
         return (channel, message)
+
+    def to_utf8(self, _string):
+        """Convert string to UTF-8 if it is unicode"""
+        if isinstance(_string, unicode):
+            _string = _string.encode("UTF-8")
+        return _string
+
+    def to_unicode(self, _string):
+        """Convert string to UTF-8 if it is unicode"""
+        if not isinstance(_string, unicode):
+            try:
+                _string = unicode(_string)
+            except:
+                try:
+                    _string = _string.decode('utf-8')
+                except:
+                    _string = _string.decode('iso-8859-1')
+        return _string
