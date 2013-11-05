859072b5d377330de0dac16b3b4a429cbb9fffe7
Riku Lindblad <riku.lindblad@gmail.com>
2013-11-05 13:37:45 +0200

    Updated module_imgur to imgur api v3

--- pyfibot/modules/available/module_imgur.py
+++ pyfibot/modules/available/module_imgur.py
@@ -1,40 +1,33 @@
 """Upload images encountered on channels to imgur.com"""
 
-import urllib
+import requests
 import logging
-import json
-try:
-    import oauth2 as oauth
-    init_ok = True
-except:
-    init_ok = False
-
-consumer = None
-token = None
-
 
 log = logging.getLogger("urltitle")
 
+album_id = ""
+access_token = ""
+refresh_token = ""
+
+init_ok = False
+
 
 def init(bot):
-    if not init_ok:
-        log.error("Module not initialized, oauth2 library not found")
-        return False
-    global config
-    global consumer_key, consumer_secret, oauth_token, oauth_token_secret
-    global token, consumer
     try:
         config = config = bot.config.get("module_imgur", {})
     except KeyError:
         config = None
 
-    consumer_key = config.get('consumer_key', '')
-    consumer_secret = config.get('consumer_secret', '')
-    oauth_token = config.get('oauth_token', '')
-    oauth_token_secret = config.get('oauth_token_secret', '')
+    global album_id
+    album_id = config.get('album_id', '')
+    global access_token
+    access_token = config.get('access_token', '')
+    global refresh_token
+    refresh_token = config.get('refresh_token', '')
 
-    token = oauth.Token(key=oauth_token, secret=oauth_token_secret)
-    consumer = oauth.Consumer(key=consumer_key, secret=consumer_secret)
+    global init_ok
+    if access_token and refresh_token:
+        init_ok = True
 
 
 # Upload all files ending in jpg and gif
@@ -44,9 +37,9 @@ def handle_url(bot, user, channel, url, msg):
     # TODO: smarter checking, using content-type perhaps?
     if url.endswith(".jpg") or url.endswith(".gif"):
         #log.info(channel, upload_images([url]))
-        images, link, aid = upload_images([url], user, channel)
-        for image in images:
-            log.info("Uploaded image http://imgur.com/%s by %s from %s to gallery %s" % (image, user, channel, link))
+        responses = upload_images([url], user, channel)
+        for r in responses:
+            log.debug("Uploaded image http://imgur.com/%s by %s from %s to gallery" % (r['id'], user, channel))
 
 
 # Upload a page of images
@@ -68,7 +61,9 @@ def upload_images(urls, user=None, channel=None):
     # uploaded images
     images = []
 
-    client = oauth.Client(consumer, token)
+    api = "https://api.imgur.com/3"
+
+    headers = {"Authorization": "Bearer %s" % access_token}
 
     # Transload image(s) to imgur
     for url in urls:
@@ -77,35 +72,33 @@ def upload_images(urls, user=None, channel=None):
         metadata = {
             'image': url,
             'type': 'url',
-            'caption': 'Original url: %s by: %s from: %s' % (url, user, channel)}
+            'album': album_id,
+            'title': 'by: %s from: %s' % (user, channel),
+            'description': 'Original url: %s' % (url)}
 
-        resp, cont = client.request("http://api.imgur.com/2/account/images.json", "POST", body=urllib.urlencode(metadata))
-        data = json.loads(cont)
-        if resp.status != 200:
-            log.debug(resp)
-            errmsg = data['error']['message']
-            log.error("Transload error for %s: %s " % (url, errmsg))
-            if errmsg == "API limits exceeded":
-                log.error("API limits exceeded, aborting uploads")
-                return
-            else:
-                continue
+        r = requests.post("%s/upload" % api, headers=headers, data=metadata)
 
-        images.append(data['images']['image']['hash'])
+        if r.status_code != 200:
+            log.error("Transload error for %s: %s " % (url, r.json()))
+            # TODO: Handle token refresh
+            # TODO: store tokens somewhere smart?
 
-    # check that the bot album exists
-    resp, cont = client.request("http://api.imgur.com/2/account/albums.json", "GET")
-    data = json.loads(cont)
-    album = [album for album in data['albums'] if album['title'] == "Pyfibot"]
-    if len(album) != 1:
-        log.error("Album not found, please create it")
-        return
+        images.append(r.json()['data'])
 
-    album = album[0]
-    aid = album['id']
-    link = album['anonymous_link']
+        log.info("%s uploaded to imgur gallery" % r.json()['data']['link'])
 
-    # add images to bot album
-    resp, cont = client.request("http://api.imgur.com/2/account/albums/%s.json" % aid, "POST", body=urllib.urlencode({'add_images': ",".join(images)}))
+    return images
 
-    return (images, link, aid)
+
+def _refresh_token(client_id, client_secret, refresh_token):
+    r = requests.post("https://api.imgur.com/oauth2/token",
+                      data={'client_id':client_id,
+                            'client_secret':client_secret,
+                            'grant_type':'refresh_token',
+                            'refresh_token':refresh_token})
+
+    new_access_token = r.json()['access_token']
+    new_refresh_token = r.json()['refresh_token']
+    log.info("Updated imgur access token for account %s" % r.json()['account_username'])
+
+    return new_access_token, new_refresh_token
