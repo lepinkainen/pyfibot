158ad30847f998afa2c4bb198757409f5a2a5f4e
Kimmo Huoman <kipenroskaposti@gmail.com>
2013-12-11 18:54:41 +0200

    Add support for module_usertrack in geoip, allowing searching by user instead of host. Serves as an example of how to use module_usertrack in other modules.

--- pyfibot/modules/module_geoip.py
+++ pyfibot/modules/module_geoip.py
@@ -4,6 +4,13 @@ import os.path
 import sys
 import socket
 
+try:
+    from modules.module_usertrack import get_table
+    user_track_available = True
+except ImportError:
+    user_track_available = False
+
+
 # http://dev.maxmind.com/geoip/legacy/geolite/
 DATAFILE = os.path.join(sys.path[0], "GeoIP.dat")
 
@@ -14,14 +21,29 @@ gi4 = pygeoip.GeoIP(DATAFILE, pygeoip.MEMORY_CACHE)
 
 
 def command_geoip(bot, user, channel, args):
-    """Determine the user's country based on host"""
+    """Determine the user's country based on host or nick, if module_usertrack is used."""
     if not args:
-        return bot.say(channel, 'usage: .geoip HOST')
+        return bot.say(channel, 'usage: .geoip HOST/NICK')
+
+    host = args
+    nick = None
+
+    if user_track_available:
+        table = get_table(bot, channel)
+        user = table.find_one(nick=args)
+        if user:
+            nick = user['nick']
+            host = user['host']
 
     try:
-        country = gi4.country_name_by_name(args)
+        country = gi4.country_name_by_name(host)
     except socket.gaierror:
         country = None
 
     if country:
-        return bot.say(channel, "%s is in %s" % (args, country))
+        if nick:
+            return bot.say(channel, "%s is in %s" % (nick, country))
+        return bot.say(channel, "%s is in %s" % (host, country))
+    if nick:
+        return bot.say(channel, 'Host not found for %s' % nick)
+    return bot.say(channel, 'Host not found for %s' % host)
