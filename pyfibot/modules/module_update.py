75e64fcb13a164354df216559feb5702d5caf956
Riku Lindblad <riku.lindblad@gmail.com>
2014-01-23 10:50:07 +0200

    Update new packages with pip when updating

--- pyfibot/modules/module_update.py
+++ pyfibot/modules/module_update.py
@@ -11,6 +11,9 @@ def command_update(bot, user, channel, args):
     if not isAdmin(user):
         return
 
+    pull_ok = False
+    pip_ok = False
+
     cmd = ['git', 'pull']
     cwd = sys.path[0]
 
@@ -21,17 +24,35 @@ def command_update(bot, user, channel, args):
     out, err = p.communicate()
 
     if res:
-        bot.say(channel, "Update failed:")
+        bot.say(channel, "Git pull failed:")
         for line in out.split("\n"):
             bot.say(channel, "%s" % line)
     else:
-        bot.say(channel, "Update OK:")
+        pull_ok = True
+        bot.say(channel, "Git update OK:")
         for line in out.split("\n"):
             bot.say(channel, "%s" % line)
-
-        # Rehash after successful update
-        bot.command_rehash(user, channel, args)
 
     # only report errors when the update failed, git uses stderr for normal output..
     if res and err:
         bot.say(channel, "Errors: %s" % err)
+
+    # fetch new required packages if needed
+    cmd = ['../bin/pip', 'install', '-r', '../requirements.txt']
+    log.debug("executing pip install in %s" % cwd)
+
+    p = subprocess.Popen(cmd, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res = p.wait()
+    out, err = p.communicate()
+
+    if res:
+        bot.say(channel, "Update failed:")
+        for line in out.split("\n"):
+            bot.say(channel, "%s" % line)
+    else:
+        bot.say(channel, "Package status OK")
+        pip_ok = True
+
+    # Rehash after successful update
+    if pip_ok and pull_ok:
+        bot.command_rehash(user, channel, args)
