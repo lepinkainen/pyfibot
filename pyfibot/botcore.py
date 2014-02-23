482b18bd568ba73bd5dd311e575034d362eec8e3
Miia 'Myrtti' Ranta <myrtti+github@gmail.com>
2014-02-24 00:04:21 +0200

    fixing errors

--- pyfibot/botcore.py
+++ pyfibot/botcore.py
@@ -225,7 +225,9 @@ class PyFiBot(irc.IRCClient, CoreCommands):
 
     def signedOn(self):
         """Called when bot has succesfully signed on to server."""
-        # QNet specific auth & ip hiding
+        log.info("Signed on to network")
+        # QuakeNet specific auth and IP-address masking
+        # TODO: Alias could be case-sensitive
         if self.network.alias == "quakenet":
             log.info("I'm on quakenet, authenticating...")
             self.mode(self.nickname, '+', 'x') # Hide ident
@@ -236,7 +238,7 @@ class PyFiBot(irc.IRCClient, CoreCommands):
             else:
                 self.say("Q@CServe.quakenet.org", "AUTH %s %s" % (authname, authpass))
                 log.info("Auth sent.")
-# more generic authentication
+	# more generic authentication
         else:
             authname = self.factory.config['networks'][self.network.alias].get('authname', None)
             authpass = self.factory.config['networks'][self.network.alias].get('authpass', None)
@@ -246,10 +248,10 @@ class PyFiBot(irc.IRCClient, CoreCommands):
                 self.say("NickServ", "IDENTIFY %s %s" % (authname, authpass))
 		log.info("Auth sent.")
 
-# allowing the connection to establish and authentication to happen before joining
+	# allowing the connection to establish and authentication to happen before joining
 	reactor.callLater(5, self.joinChannels)
 
-# separate function to allow timing the joins
+    # separate function to allow timing the joins
     def joinChannels(self):
         for chan in self.network.channels:
             # Defined as a tuple, channel has a key
