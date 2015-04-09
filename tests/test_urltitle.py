8a64d2eb0c46aeb6049e6503e8016f2e81b89978
aapa <aapa@users.noreply.github.com>
2015-04-10 02:07:05 +0300

    Updated YouTube handler to use v3 API.

--- tests/test_urltitle.py
+++ tests/test_urltitle.py
@@ -70,7 +70,7 @@ def test_imdb():
 
 
 def test_youtube():
-    regex = 'Title: (.*?) by (.*?) \[%s - \[\** *\] - %s views - %s( - XXX)?\]' % (lengh_str_regex, views_str_regex, age_str_regex)
+    regex = 'Title: (.*?) by (.*?) \[%s - %s views - %s( - age restricted)?\]' % (lengh_str_regex, views_str_regex, age_str_regex)
     msg = "http://www.youtube.com/watch?v=awsolTK175c"
     module_urltitle.init(bot)
     check_re(regex, module_urltitle.handle_url(bot, None, "#channel", msg, msg)[1])
