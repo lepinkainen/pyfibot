685cb6add281455d05699f6279ae29690b677a8a
Riku Lindblad <riku.lindblad@gmail.com>
2015-12-28 22:44:24 +0200

    Update test_areena to pytest Fix tv test case by changing the url to a more standard one Use pytest fixtures to make the code less boilerplate-y

--- tests/test_areena.py
+++ tests/test_areena.py
@@ -3,11 +3,17 @@ import bot_mock
 from pyfibot.modules import module_urltitle
 from utils import check_re
 
+import pytest
 from vcr import VCR
 my_vcr = VCR(path_transformer=VCR.ensure_suffix('.yaml'),
-             cassette_library_dir="tests/cassettes/")
+             cassette_library_dir="tests/cassettes/",
+             record_mode="once")
 
-bot = bot_mock.BotMock()
+@pytest.fixture
+def botmock():
+    bot = bot_mock.BotMock()
+    module_urltitle.init(bot)
+    return bot
 
 lengh_str_regex = u'\d+(h|m|s)(\d+(m))?(\d+s)?'
 views_str_regex = u'\d+(\.\d+)?(k|M|Billion|Trillion)?'
@@ -15,31 +21,28 @@ age_str_regex = u'(FRESH|(\d+(\.\d+)?(y|d) ago))'
 
 
 @my_vcr.use_cassette
-def test_areena_radio():
+def test_areena_radio(botmock):
     regex = u'Title: (.*?) \[%s - %s plays - %s( - exits in \d+ (weeks|days|hours|minutes))?\]' % (lengh_str_regex, views_str_regex, age_str_regex)
     msg = "http://areena.yle.fi/1-2006973"
-    module_urltitle.init(bot)
-    check_re(regex, module_urltitle.handle_url(bot, None, "#channel", msg, msg)[1])
+    check_re(regex, module_urltitle.handle_url(botmock, None, "#channel", msg, msg)[1])
 
-# @my_vcr.use_cassette
-# def test_areena_tv():
-#     regex = u'Title: (.*?) \[%s - %s plays - %s( - exits in \d+ (weeks|days|hours|minutes))?\]' % (lengh_str_regex, views_str_regex, age_str_regex)
-#     msg = "http://areena.yle.fi/1-1999860"
-#     module_urltitle.init(bot)
-#     check_re(regex, module_urltitle.handle_url(bot, None, "#channel", msg, msg)[1])
+@my_vcr.use_cassette
+def test_areena_tv(botmock):
+    regex = u'Title: (.*?) \[%s - %s plays - %s( - exits in \d+ (weeks|days|hours|minutes))?\]' % (lengh_str_regex, views_str_regex, age_str_regex)
+    #msg = "http://areena.yle.fi/1-1999860"
+    msg = "http://areena.yle.fi/1-3210197"
+    check_re(regex, module_urltitle.handle_url(botmock, None, "#channel", msg, msg)[1])
 
 
 @my_vcr.use_cassette
-def test_areena_series():
+def test_areena_series(botmock):
     regex = u'Title: (.*?) \[SERIES - \d+ episodes - latest episode: %s\]' % (age_str_regex)
     msg = "http://areena.yle.fi/1-2540167"
-    module_urltitle.init(bot)
-    check_re(regex, module_urltitle.handle_url(bot, None, "#channel", msg, msg)[1])
+    check_re(regex, module_urltitle.handle_url(botmock, None, "#channel", msg, msg)[1])
 
 
 @my_vcr.use_cassette
-def test_areena_live():
+def test_areena_live(botmock):
     regex = u'Title: Yle TV2 - (.*?)( <http://areena.yle.fi/\d-\d+>)? \(LIVE\)'
     msg = "http://areena.yle.fi/tv/suora/tv2"
-    module_urltitle.init(bot)
-    check_re(regex, module_urltitle.handle_url(bot, None, "#channel", msg, msg)[1])
+    check_re(regex, module_urltitle.handle_url(botmock, None, "#channel", msg, msg)[1])
