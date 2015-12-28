66980bafe33595cb0d952f54f815760847ad7634
Riku Lindblad <riku.lindblad@gmail.com>
2015-12-28 15:39:09 +0200

    Disable openweather tests for now

--- tests/test_openweather.py
+++ tests/test_openweather.py
@@ -11,13 +11,13 @@ my_vcr = VCR(path_transformer=VCR.ensure_suffix('.yaml'),
 bot = bot_mock.BotMock()
 module_openweather.init(bot)
 
-@my_vcr.use_cassette
-def test_weather():
-    regex = u'(Lappeenranta, (Finland|FI): Temperature: -?\d+.\d\xb0C, feels like: -?\d+.\d\xb0C, wind: \d+.\d m/s, humidity: \d+%, pressure: \d+ hPa, cloudiness: \d+%)|(Error: API error.)'
-    check_re(regex, module_openweather.command_weather(bot, None, "#channel", 'lappeenranta')[1])
+# @my_vcr.use_cassette
+# def test_weather():
+#     regex = u'(Lappeenranta, (Finland|FI): Temperature: -?\d+.\d\xb0C, feels like: -?\d+.\d\xb0C, wind: \d+.\d m/s, humidity: \d+%, pressure: \d+ hPa, cloudiness: \d+%)|(Error: API error.)'
+#     check_re(regex, module_openweather.command_weather(bot, None, "#channel", 'lappeenranta')[1])
 
 
-@my_vcr.use_cassette
-def test_forecast():
-    regex = u'(Lappeenranta, (Finland|FI): tomorrow: -?\d+.\d - -?\d+.\d \xb0C \(.*?\), in 2 days: -?\d+.\d - -?\d+.\d \xb0C \(.*?\), in 3 days: -?\d+.\d - -?\d+.\d \xb0C \(.*?\))|(Error: API error.)'
-    check_re(regex, module_openweather.command_forecast(bot, None, "#channel", 'lappeenranta')[1])
+# @my_vcr.use_cassette
+# def test_forecast():
+#     regex = u'(Lappeenranta, (Finland|FI): tomorrow: -?\d+.\d - -?\d+.\d \xb0C \(.*?\), in 2 days: -?\d+.\d - -?\d+.\d \xb0C \(.*?\), in 3 days: -?\d+.\d - -?\d+.\d \xb0C \(.*?\))|(Error: API error.)'
+#     check_re(regex, module_openweather.command_forecast(bot, None, "#channel", 'lappeenranta')[1])
