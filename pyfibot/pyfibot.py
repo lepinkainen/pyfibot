9c931643b0fde7cfb1437fd6b1cfb636be726524
Riku Lindblad <riku.lindblad@iki.fi>
2020-05-31 21:42:06 +0300

    Move get_url to botcore
    Handle URL size warnings in core instead of a module

--- pyfibot/pyfibot.py
+++ pyfibot/pyfibot.py
@@ -14,7 +14,6 @@ from __future__ import print_function, division
 import sys
 import os.path
 import time
-import requests
 import fnmatch
 import logging
 import logging.handlers
@@ -274,55 +273,6 @@ class PyFiBotFactory(ThrottledClientFactory):
         # g["to_utf8"] = self.to_utf8
         # g["to_unicode"] = self.to_unicode
         return g
-
-    def get_url(self, url, nocache=False, params=None, headers=None, cookies=None):
-        return self.getUrl(url, nocache, params, headers, cookies)
-
-    def getUrl(self, url, nocache=False, params=None, headers=None, cookies=None):
-        """Gets data, bs and headers for the given url, using the internal cache if necessary"""
-
-        browser = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:49.0) Gecko/20100101 Firefox/49.0"
-        # Common session for all requests
-        s = requests.session()
-        s.stream = True  # Don't fetch content unless asked
-        s.headers.update({"User-Agent": browser})
-        s.headers.update({"Accept-Language": "*"})
-        s.headers.update(
-            {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"})
-        # Custom headers from requester
-        if headers:
-            s.headers.update(headers)
-        # Custom cookies from requester
-        if cookies:
-            s.cookies.update(cookies)
-
-        try:
-            r = s.get(url, params=params, timeout=5)
-        except requests.exceptions.InvalidSchema:
-            log.error("Invalid schema in URI: %s" % url)
-            return None
-        except requests.exceptions.SSLError:
-            log.error("SSL Error when connecting to %s" % url)
-            return None
-        except requests.exceptions.ConnectionError:
-            log.error("Connection error when connecting to %s" % url)
-            return None
-
-        size = int(r.headers.get("Content-Length", 0)) // 1024
-
-        if size > 2048:
-            log.warn("Content too large, will not fetch: %skB %s" %
-                     (size, url))
-            return None
-
-        return r
-
-    def getNick(self, user):
-        """Parses nick from nick!user@host
-        @type user: string
-        @param user: nick!user@host
-        @return: nick"""
-        return user.split("!", 1)[0]
 
     def getIdent(self, user):
         """Parses ident from nick!user@host
